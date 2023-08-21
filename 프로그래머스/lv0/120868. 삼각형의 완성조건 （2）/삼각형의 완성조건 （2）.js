55468efab7fe40711de438377b0a981f369af80e
boyeonJ <32887635+boyeonJ@users.noreply.github.com>
2023-08-21 15:25:49 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,8 +1,11 @@
 function solution(sides) {
     sides.sort((a,b)=>a-b);
     
-    const noLongest = sides[1] - (sides[1]-sides[0]);
-    const longest = (sides[1]+sides[0]) - sides[1] - 1;
+    // const noLongest = sides[1] - (sides[1]-sides[0]);
+    // const longest = (sides[1]+sides[0]) - sides[1] - 1;
     
-    return noLongest + longest;
+    // sides[1] - (sides[1]-sides[0]) +  (sides[1]+sides[0]) - sides[1] - 1 = sides[0]*2 -1
+    
+    // return noLongest + longest;
+    return sides[0]*2 -1
 }
